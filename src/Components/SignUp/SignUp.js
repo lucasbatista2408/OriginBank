927eef675b8902e0cacdb0a166b7cf98d7b9bc40
Lucas Batista <lucasbatista2408@gmail.com>
2022-09-27 22:21:07 -0300

    signup component added with media query

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,58 +1,19 @@
-import {React, useState} from "react"
-import { useNavigate } from "react-router-dom"
-import axios from "axios"
-import styled from "styled-components"
+import React from "react";
+import MediaQuery from "react-responsive";
+import SignUpDesktop from "./SignUpDesktop";
+import SignUpMobile from "./SignUpMobile";
+
 
 export default function SignUp(){
 
-  const navigate = useNavigate();
-
-  const [form, setForm] = useState({
-    username: '',
-    email: '',
-    password: '',
-    picture: ''
-  })
-
-function SignUp(e){
-  e.preventDefault();
-  e.currentTarget.disabled=true;
-
-  if(!form.username || !form.email || !form.password || !form.picture){
-    return alert('Fill all the necessary fields')
-  }
-
-  const URI = process.env.REACT_APP_DATABASE_URI
-  const URL = `${URI}/signup`
-  const signUp = form;
-  const promise = axios.post(URL, signUp)
-  promise
-  .then( res => {
-    console.log(res.data)
-    navigate('/')
-  })
-  .catch(error => (
-    alert(HandleError(error.response)),
-    e.currentTarget.disabled=false,
-    window.location.reload(true)
-  ))
-}
-
-function HandleError(error){
-
-  if(error.status === 409){
-    return error.data
-  } else {
-    return 'something went wront'
-  }
-
-}
-
-function handleClick(){
-  navigate('/')
-}
-
-  return(
-    <h1>HelloWorld</h1>
-  )
+    return(
+        <>
+        <MediaQuery maxWidth={1023}>
+            <SignUpMobile/>
+        </MediaQuery>
+        <MediaQuery minWidth={1024}>
+            <SignUpDesktop/>
+        </MediaQuery>
+        </>
+    )
 }
---
+++ src/Components/SignUp/SignUpDesktop.js
@@ -0,0 +1,58 @@
+import {React, useState} from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
+import styled from "styled-components"
+
+export default function SignUpDesktop(){
+
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: '',
+    picture: ''
+  })
+
+function SignUp(e){
+  e.preventDefault();
+  e.currentTarget.disabled=true;
+
+  if(!form.username || !form.email || !form.password || !form.picture){
+    return alert('Fill all the necessary fields')
+  }
+
+  const URI = process.env.REACT_APP_DATABASE_URI
+  const URL = `${URI}/signup`
+  const signUp = form;
+  const promise = axios.post(URL, signUp)
+  promise
+  .then( res => {
+    console.log(res.data)
+    navigate('/')
+  })
+  .catch(error => (
+    alert(HandleError(error.response)),
+    e.currentTarget.disabled=false,
+    window.location.reload(true)
+  ))
+}
+
+function HandleError(error){
+
+  if(error.status === 409){
+    return error.data
+  } else {
+    return 'something went wront'
+  }
+
+}
+
+function handleClick(){
+  navigate('/')
+}
+
+  return(
+    <h1>HelloWorld</h1>
+  )
+}
