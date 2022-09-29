5d839b513da11816b2503d54bcd044d48a604fc7
lucas <lucasbatista2408@gmail.com>
2022-09-29 04:08:26 -0300

    imports userContext  and Dashboard and creates route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from "react"
+import {React, useState} from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Welcome from "./Components/WelcomePage/Welcome";
 import SignUp from "./Components/SignUp/SignUp";
 import SignIn from "./Components/SignIn/SignIn";
+import UserContext from "./context";
+import DashBoard from "./Components/Dashboard/Dashboard";
 
 function App() {
-  return (<>
+
+  const [info, setInfo] = useState({}); // SALVA O NOME DE USUARIO E TOKEN QUE VEM DO BACK
+  const [local, setLocal] = useState({}); // SALVA O TOKEN QUE VEM DO LOCAL STORAGE
+  const contextValue = { info, setInfo, local, setLocal }
+
+  return (
+    <>
+    <UserContext.Provider value={contextValue}>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<DashBoard />} />
       </Routes>
     </BrowserRouter>
+    </UserContext.Provider>
     </>
   );
 }
